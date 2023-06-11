9ee6cb44f53f66f6d9c0a97864e67a0006e19c51
Hanqi Dai <hanqid@andrew.cmu.edu>
2023-06-11 00:07:47 -0700

    django created

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# Use the official Python image as the base image
+FROM python:3
+
+# Set environment variables
+ENV PYTHONUNBUFFERED 1
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Install Django
+RUN pip install Django==3.2.4
---
+++ app/asl_text_speech/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for asl_text_speech project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asl_text_speech.settings')
+
+application = get_asgi_application()
