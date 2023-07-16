53bd1edb011a74fe3c9ae4a10dde5cf0e2910683
TheOnlyWei24 <125891913+TheOnlyWei24@users.noreply.github.com>
2023-07-16 17:15:27 -0400

    Tweak to video processing and new weights

--- WLASL_Inference/Inference.py
+++ WLASL_Inference/Inference.py
@@ -5,6 +5,7 @@ import cv2
 from pytorch_i3d import InceptionI3d
 import torch.nn as nn
 import torch.nn.functional as nnf
+from torchinfo import summary
 
 def pad(img):
     max_size = max(img.shape)
@@ -13,6 +14,15 @@ def pad(img):
     left = int((max_size-img.shape[1])/2)
     right = int((max_size-img.shape[1])/2)
     return cv2.copyMakeBorder(img, top, bot, left, right, cv2.BORDER_CONSTANT, None, value = 0)
+
+def center_crop(img):
+    center = img.shape
+    w = 224
+    h = 224
+    x = center[1]/2 - w/2
+    y = center[0]/2 - h/2
+
+    return img[int(y):int(y+h), int(x):int(x+w)]
 
 def process_sequence(frames):
     np_frames = np.asarray(frames, dtype=np.float32)
@@ -31,6 +41,7 @@ def load_model(weights, num_classes):
     i3d = InceptionI3d(400, in_channels=3)
     i3d.replace_logits(num_classes)
     i3d.load_state_dict(torch.load(weights))
+    summary(i3d)
     i3d.cuda()
     i3d = nn.DataParallel(i3d)
     i3d.eval()
@@ -45,34 +56,41 @@ if __name__ == '__main__':
     all_words = file[1].tolist()
     glosses = all_words[:num_classes]
 
-    weights = 'weights/nslt_100.pt'
+    weights = 'weights/nslt_100_006900_0.815436.pt'
     i3d = load_model(weights, num_classes)
 
     sequence = []
     sentence = []
-    threshold = 0.80
+    threshold = 0.85
     word = ""
 
     cap = cv2.VideoCapture(0)
+    #cap = cv2.VideoCapture('video.mp4')
+
     while cap.isOpened():
 
         # Read feed
         ret, frame = cap.read()
+        
+        if ret == 0:
+            break
+            
         output_frame = frame.copy()
         
         frame = pad(frame)
-        frame = cv2.resize(frame, dsize=(224, 224))
-        
+        frame = cv2.resize(frame, dsize=(256, 256))
+        frame = center_crop(frame)
+
         frame = (frame / 255.) * 2 - 1
 
         sequence.append(frame)
-        sequence = sequence[-30:]
-        
-        if len(sequence) == 30:
+        sequence = sequence[-40:]
+
+        if len(sequence) == 40:
             per_frame_logits = i3d(process_sequence(sequence))
             predictions = torch.max(per_frame_logits, dim=2)[0]
             word = glosses[torch.argmax(predictions[0]).item()]
-
+            
             if torch.max(nnf.softmax(predictions, dim=1)).item() > threshold: 
                 if len(sentence) > 0: 
                     if word != sentence[-1]:
@@ -83,14 +101,15 @@ if __name__ == '__main__':
                 if len(sentence) > 5: 
                     sentence = sentence[-5:]
                 
-        # Render output
         cv2.rectangle(output_frame, (0,0), (640, 40), (245, 117, 16), -1)
-        cv2.putText(output_frame, ' '.join(sentence), (3,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(output_frame, ' '.join(sentence), (3,30), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Show to screen
         cv2.imshow('OpenCV Feed', output_frame)
+        print(sentence, end='\r')
 
-        # Close window
+        # Break
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
