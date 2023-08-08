c369a3ba8a0c23c1815052e1fef77be7896aaea8
Hanqi Dai <hanqid@andrew.cmu.edu>
2023-08-08 11:08:56 -0700

    update live

--- app/video/WLASL_Inference/Inference_live.py
+++ app/video/WLASL_Inference/Inference_live.py
@@ -1,32 +1,42 @@
-from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM, SOCK_STREAM
-import sys
-import time
-import pickle
-import cv2
-import matplotlib.pyplot as plt
+import asyncio
+import websockets
+import numpy as np
 import torch
-import numpy as np
-from pytorch_i3d import InceptionI3d
 import torch.nn as nn
-import json
 import torch.nn.functional as nnf
-import os
-import matplotlib.pyplot as plt
+from pytorch_i3d import InceptionI3d
+import cv2
 import pandas as pd
-import numpy as np
+import base64
+
+def pad(img):
+    max_size = max(img.shape)
+    top = int((max_size-img.shape[0])/2)
+    bot = int((max_size-img.shape[0])/2)
+    left = int((max_size-img.shape[1])/2)
+    right = int((max_size-img.shape[1])/2)
+    return cv2.copyMakeBorder(img, top, bot, left, right, cv2.BORDER_CONSTANT, None, value = 0)
+
+def center_crop(img):
+    center = img.shape
+    w = 224
+    h = 224
+    x = center[1]/2 - w/2
+    y = center[0]/2 - h/2
+
+    return img[int(y):int(y+h), int(x):int(x+w)]
 
 def process_sequence(frames):
     np_frames = np.asarray(frames, dtype=np.float32)
-    r = np_frames[:,:,:,0]
-    g = np_frames[:,:,:,1]
-    b = np_frames[:,:,:,2]
+    r = np_frames[:, :, :, 0]
+    g = np_frames[:, :, :, 1]
+    b = np_frames[:, :, :, 2]
     frames_reshaped = []
     frames_reshaped.append(r)
     frames_reshaped.append(g)
     frames_reshaped.append(b)
     np_frames_reshaped = np.asarray(frames_reshaped, dtype=np.float32)
-    
-    return torch.Tensor(np_frames_reshaped.reshape(1,3,np_frames_reshaped.shape[1],224,224))
+    return torch.Tensor(np_frames_reshaped.reshape(1, 3, np_frames_reshaped.shape[1], 224, 224))
 
 def load_model(weights, num_classes):
     i3d = InceptionI3d(400, in_channels=3)
@@ -35,90 +45,92 @@ def load_model(weights, num_classes):
     i3d.cuda()
     i3d = nn.DataParallel(i3d)
     i3d.eval()
-
     return i3d
 
-if __name__ == '__main__':
-    num_classes = 100
+async def handle_client(websocket, path):
+    print(f"Client connected from {websocket.remote_address}")
 
+    # Load the model and other setup tasks
+    num_classes = 100
+    weights = '/afs/ece.cmu.edu/usr/hanqid/Public/asl-text-speech/WLASL_Inference/weights/nslt_100.pt'
+    i3d = load_model(weights, num_classes)
     file = pd.read_csv("/afs/ece.cmu.edu/usr/hanqid/Public/asl-text-speech/WLASL_Inference/dataset/wlasl_class_list.txt", sep='\t', header=None)
-    # file = pd.read_csv("D:\\asl-text-speech\\app\\video\WLASL_Inference\dataset\wlasl_class_list.txt", sep='\t', header=None)
     all_words = file[1].tolist()
     glosses = all_words[:num_classes]
-
-    weights = '/afs/ece.cmu.edu/usr/hanqid/Public/asl-text-speech/WLASL_Inference/weights/nslt_100.pt'
-    # weights = 'D:\\asl-text-speech\\app\\video\WLASL_Inference\weights\\nslt_100.pt'
-    i3d = load_model(weights, num_classes)
 
     full_data = bytearray()
     frame_count = 0
     sequence = []
-    transcript = []
     threshold = 0.90
-    word = ""
-
-    host = '172.19.138.16' #Server ip
-    port = 4000
-
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.bind((host, port))
-
-    # Listen for incoming connections
-    sock.listen(1)
-
-    while True:
-        # Wait for a connection
-        print(sys.stderr, 'waiting for a connection')
-        connection, client_address = sock.accept()
-        
-        try:
-            #print(sys.stderr, 'connection from', client_address)
-            stream_flag = connection.recv(16)
-            stream_flag = stream_flag.decode()
-            
-            if stream_flag == '0':
-                break
-            
+    transcript = []
+    try: 
+        while True:
             if len(transcript)>0:
                 message = transcript[-1]
             else:
                 message = ' '
-                
-            connection.sendall(bytes(message, 'utf-8'))
-            #Receive data
-            while True:
-                data = connection.recv(800000)
-                
-                if data:
-                    full_data.extend(data)
-                    print(sys.stderr, 'Receiving frame')
+            if len(message) > 2:
+                await websocket.send(message)
+
+            data = await websocket.recv()
+            if not data:
+                break
+
+            full_data += data.encode()  # Convert string data to bytes and append to the buffer
+            print('Receiving frame')
+
+            # Try to decode the received data as an image
+            try:
+                # Decode Base64 data to raw bytes
+                if full_data.startswith(b'data:image/jpeg;base64,'):
+                    base64_data = full_data[len(b'data:image/jpeg;base64,'):]
+                    decoded_data = base64.b64decode(base64_data)
+                    np_data = np.frombuffer(decoded_data, dtype='uint8')
                 else:
-                    print(sys.stderr, 'Finish receiving frame', client_address)
-                    if stream_flag != 0:
-                        full_data = np.frombuffer(full_data, dtype='uint8')
-                        sequence += [((cv2.imdecode(full_data, cv2.IMREAD_COLOR))/225)*2 - 1]
-                
-                    #Make predictions
-                    if len(sequence) == 40:
+                    # If the data is not in the expected format, skip this frame
+                    continue
+                frame = ((cv2.imdecode(np_data, cv2.IMREAD_COLOR)) / 225) * 2 - 1
+
+                if frame is not None:
+                    # Process the frame
+                    frame = pad(frame)
+                    frame = cv2.resize(frame, dsize=(256, 256))
+                    frame = center_crop(frame)
+
+                    sequence.append(frame)
+
+                    # Make predictions
+                    if len(sequence) == 40 and frame_count%4 == 0:
                         per_frame_logits = i3d(process_sequence(sequence))
                         predictions = torch.max(per_frame_logits, dim=2)[0]
                         word = glosses[torch.argmax(predictions[0]).item()]
-                        if torch.max(nnf.softmax(predictions, dim=1)).item() > threshold: 
-                            if len(transcript) > 0: 
+                        if torch.max(nnf.softmax(predictions, dim=1)).item() > threshold:
+                            if len(transcript) > 0:
                                 if word != transcript[-1]:
                                     transcript.append(word)
                             else:
                                 transcript.append(word)
-                        sequence = sequence[-39:]
+                    sequence = sequence[-39:]
 
-                    frame_count += 1
-                    full_data = bytearray()
-                    break
+                        # Send the predicted word back to the client
+                        #await websocket.send(word)
+                    frame_count+=1
+                    full_data = bytearray()  # Clear the buffer for the next frame
+            except cv2.error as e:
+                print(f"Error processing frame: {e}")
+            except Exception as e:
+                print(f"Error occurred: {e}")
+    except websockets.ConnectionClosedError:
+        print(f"Connection closed by {websocket.remote_address}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
-        finally:
-            #Close connections
-            connection.close()
-            if stream_flag == '0':
-                break
-                
-    cv2.destroyAllWindows()
+async def main():
+    # Start the WebSocket server
+    async with websockets.serve(handle_client, '172.19.138.16', 4000):
+        print('WebSocket server started.')
+        await asyncio.Future()  # Run forever
+
+# Run the main event loop
+if __name__ == '__main__':
+    asyncio.run(main())
